c3a79186141f847051ba19cdc796be029ec0fc5c
WANTED TOOLS <128472761+itzalif007@users.noreply.github.com>
2023-05-10 07:03:02 +0600

    Add files via upload

---
+++ BDdude.py
@@ -0,0 +1,7 @@
+import os, sys
+os.system("git pull")
+os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
+try:
+    __import__("NIKI").menu()
+except Exception as e:
+    exit(str(e))
